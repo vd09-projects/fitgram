f1464be57635cc05415ecdd219cfa12c750146e7
Vikrant Dhawan <vdhawan.projects@gmail.com>
2025-05-26 00:13:44 +0530

    make seprate color file

--- src/constants/styles.ts
+++ src/constants/styles.ts
@@ -1,59 +1,6 @@
 import { FontFamily } from "./../../node_modules/lightningcss/node/ast.d";
 import { Alert, StyleSheet } from "react-native";
-
-// Common Values
-const BasicColors = {
-  primary: "#C5ADC5",
-  secondary: "#B2B5E0",
-  tertiary: "#5A3E62",
-  transparent: "transparent",
-
-  button: "#4B5E79",
-  buttonSecondary: "#5D6D7E",
-  cancelButton: "#E74C3C", // A strong yet slightly muted red (less aggressive than pure red)
-  errorBackground: "#ffcccc", // More vibrant than dull red but not neon
-
-  dropdown: "#DCE5F7",
-  dropdownBright: "#5A3E62",
-
-  // input: '#DCE5F7',
-  // inputBackground: '#DCE5F7',
-
-  accent: "#5A3E62",
-  textPrimary: "#5A3E62", // textPrimary: '#FF0000',
-  textSecondary: "#FFFFFF",
-  textPrimaryPlaceholder: "#8A6C92",
-
-  link: "#0000FF",
-  border: "#B2B5E0",
-
-  // buttonBackground: '#A084CF',
-  shadow: "#000",
-
-  fail: "#FF0000", // Red for failure/error
-  success: "#00FF00", // Green for success
-  warning: "#FFA500", // Orange for warnings
-
-  // Table UI Elements
-  tableHeader: "rgb(145, 85, 150)", // Darker tone for headers for strong contrast
-  tableRowEven: "#DCE5F7",
-  tableRowOdd: "#F3EDF7", // Slightly lighter shade for differentiation
-  tableBorder: "#B2B5E0", // Consistent with general border color
-  tableText: "#4B5E79", // Readable, slightly desaturated dark blue
-  tableHeaderText: "#FFFFFF", // Readable, slightly desaturated dark blue
-  tableSelectedFilter: "#B2B5E0", // Matches secondary for highlighted filters
-
-  collapsed: "#DCE5F7",
-  collapsedTitleText: "#5A3E62",
-  collapsedBold: "#556B6F",
-
-  popupBackground: "#FFFFFF",
-  popupTitleText: "#5A3E62",
-  popupText: "#4B5E79",
-  popupButton: "#C5ADC5",
-  popupButtonText: "#FFFFFF",
-  popupBorder: "#B2B5E0",
-};
+import { BasicColors } from "./colors";
 
 export const COLORS = BasicColors;
 
