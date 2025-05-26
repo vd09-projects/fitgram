4027172b62336c1ff041dd42f1b1463f6a625979
Vikrant Dhawan <vdhawan.projects@gmail.com>
2025-05-27 03:14:17 +0530

    creating HinataHyugaColors

--- src/constants/colors.ts
+++ src/constants/colors.ts
@@ -66,3 +66,71 @@ export const BasicColors = {
   popupButtonText: "#FFFFFF",
   popupBorder: "#B2B5E0",
 };
+
+export const HinataHyugaColors = {
+  primary: "#3C3F5C",
+  secondary: "#E7EBE9",
+  // secondary: "#E7EBE9",
+  tertiary: "rgb(145, 85, 150)",
+
+  transparent: "transparent",
+
+  button: "#5B6590",
+  buttonSecondary: "#B5A3CF",
+  buttonText: "#EADDF0",
+  cancelButton: "#973740", // A strong yet slightly muted red (less aggressive than pure red)
+  errorBackground: "#ffcccc", // More vibrant than dull red but not neon
+
+  dropdown: "#DCE5F7",
+  dropdownBright: "#5A3E62",
+  dropdownItemBackground: "#5B6590",
+  dropdownInputPlaceholder: "#B5A3CF",
+  dropdownInputText: "#5A3E62",
+
+  cardBackground: "#B5A3CF",
+  cardHeader: "#3C3F5C",
+
+  inputPrimaryText: '#5A3E62',
+  inputPrimaryBackground: 'transparent',
+  inputPrimaryPlaceholder: "#B5A3CF",
+  inputSecondaryBackground: '#EADDF0',
+  inputBorder: "#B5A3CF",
+
+  switchTrue: "#82976B",
+  switchFalse: "#668CAF",
+
+  accent: "#5A3E62",
+  textPrimary: "#B5A3CF",
+  textSecondary: "#EADDF0",
+  textPrimaryPlaceholder: "#8A6C92",
+
+  link: "#0000FF",
+  border: "#B2B5E0",
+
+  // buttonBackground: '#A084CF',
+  shadow: "#E7EBE9",
+
+  fail: "#FF0000", // Red for failure/error
+  success: "#00FF00", // Green for success
+  warning: "#FFA500", // Orange for warnings
+
+  // Table UI Elements
+  tableHeader: "#B5A3CF", // Darker tone for headers for strong contrast
+  tableRowEven: "#E5EFF1",
+  tableRowOdd: "#F2F7F8", // Slightly lighter shade for differentiation || CCE0E5
+  tableBorder: "#B2B5E0", // Consistent with general border color
+  tableText: "#4B5E79", // Readable, slightly desaturated dark blue
+  tableHeaderText: "#FFFFFF", // Readable, slightly desaturated dark blue
+  tableSelectedFilter: "#B2B5E0", // Matches secondary for highlighted filters
+
+  collapsed: "#FEFEFE",
+  collapsedTitleText: "#B5A3CF",
+  collapsedBold: "#556B6F",
+
+  popupBackground: "#FFFFFF",
+  popupTitleText: "#5A3E62",
+  popupText: "#4B5E79",
+  popupButton: "#C5ADC5",
+  popupButtonText: "#FFFFFF",
+  popupBorder: "#B2B5E0",
+};
